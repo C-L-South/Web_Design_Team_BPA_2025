1b5cf47745f8e3a4d4b8ac05e36ad8562d715cd9
C-L-South <codyli9219@gmail.com>
2025-12-07 01:13:26 -0600

    Add Firebase configuration and authentication handling

---
+++ js/information.js
@@ -0,0 +1,23 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDncN-lZWQLBa3fkMI3QkAE7CqcA6IK3ss",
+  authDomain: "bpa-webdesign-team.firebaseapp.com",
+  databaseURL: "https://bpa-webdesign-team-default-rtdb.firebaseio.com",
+  projectId: "bpa-webdesign-team",
+  storageBucket: "bpa-webdesign-team.firebasestorage.app",
+  messagingSenderId: "12413897311",
+  appId: "1:12413897311:web:b6a1b9363d3e6f2a0b69ea",
+  measurementId: "G-2D29B9KZCR"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log("User is still logged in:", user.email);
+
+  } else {
+    console.log("No user logged in — redirecting");
+    window.location.href = "login.html";
+  }
+});
